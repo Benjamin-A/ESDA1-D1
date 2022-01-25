9318371fc9841e23cc9d50f4b8ceec9a5bef5a48
Benjamin-A <47823558+Benjamin-A@users.noreply.github.com>
2022-01-26 00:02:45 +0100

    Update design 1.py

--- design 1.py
+++ design 1.py
@@ -2,8 +2,8 @@ import numpy as np
 def A (x):
   return 10**(x/20)
 
-Amin = A(-5.1)
-Amax = A(-36.1)
+Amin = A(-5)
+Amax = A(-36)
 R = 10000
 
 R1 = (-R+R*Amax)/(Amax-Amin)-R
@@ -12,8 +12,9 @@ print (f"Optimal R1: {R1}")
 R2 = (-R*Amax)/(Amax-Amin)
 print (f"Optimal R2: {R2}\n")
 
-R1=8000
-R2=293
+R=10000
+R1=7840
+R2=290
 
 Amin2=(R2 + R)/(R1+R2+R)
 Amax2=(R2)/(R1+R2+R)
@@ -23,3 +24,13 @@ print(f"Teoretisk reell max demping: {20*np.log10(Amax2)}\n")
 
 print(f"Teoretisk min dempet v2: {Amin2*5}")
 print(f"Teoretisk max dempet v2: {Amax2*5}\n")
+
+v1 = 5.16
+v3min = 2.94 
+v3max = 0.082
+
+Amin3 = v3min/v1
+Amax3 = v3max/v1
+
+print(f"Faktisk reell min demping: {20*np.log10(Amin3)}")
+print(f"Faktisk reell max demping: {20*np.log10(Amax3)}")
